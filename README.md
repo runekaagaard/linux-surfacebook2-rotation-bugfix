4e4cd7eaf1f7dc787e603eeaa941e6ffb3c97a98
Rune Kaagaard <rumi.kg@gmail.com>
2019-08-18 00:01:23 +0200

    Correcting install file.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # linux-surfacebook2-rotation-bugfix
+
 Ubuntu 19.04, Surface Book 2 15": Workaround for bug in jakedays / qzeds surface-linux that makes touch still work correctly when the screen is rotated.
+
+# Installation
+git clone https://github.com/runekaagaard/linux-surfacebook2-rotation-bugfix.git
+cd linux-surfacebook2-rotation-bugfix
+sudo ./install.sh
--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+mkdir -p ~/.config/autostart
+cp linux-surfacebook2-rotation-bugfix.desktop ~/.config/autostart/
 
-cp linux-surfacebook2-rotation-bugfix.sh /opt/
-cp linux-surfacebook2-rotation-bugfix.service /etc/systemd/system/
-systemctl enable linux-surfacebook2-rotation-bugfix 
+sudo cp -a linux-surfacebook2-rotation-bugfix.sh /usr/bin/
+echo "Login and out again, and touchpad on screen rotation should start working."

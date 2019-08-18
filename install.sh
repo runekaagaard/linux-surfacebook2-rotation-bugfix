4c9251853196b062f0689d00a7ca7b5ec02c05ba
Rune Kaagaard <rumi.kg@gmail.com>
2019-08-18 11:58:25 +0200

    screen not pad.

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ mkdir -p ~/.config/autostart
 cp linux-surfacebook2-rotation-bugfix.desktop ~/.config/autostart/
 
 sudo cp -a linux-surfacebook2-rotation-bugfix.sh /usr/bin/
-echo "Login and out again, and touchpad on screen rotation should start working."
+echo "Login and out again, and touchscreen touch on screen rotation should start working."

59eac40548877189e0e0abf5fdb8d44dc4012412
Rune Kaagaard <rumi.kg@gmail.com>
2019-08-18 11:08:20 +0200

    Docs. Less noise in syslog.

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # linux-surfacebook2-rotation-bugfix
 
-Ubuntu 19.04, Surface Book 2 15": Workaround for bug in jakedays / qzeds surface-linux that makes touch still work correctly when the screen is rotated.
+This makes the touchpad work correctly when the screen is rotated on the following system:
+
+- Ubuntu 19.04
+- Surface Book 2 15"
+- Gnome
+- Setup.sh: https://github.com/qzed/linux-surface/commit/51c7f7a23419f4262bc7bf1b60d4e7e9830df0b7
+- Kernel: https://github.com/qzed/linux-surface/releases/tag/v5.2.5-pre1 
 
 # Installation
-git clone https://github.com/runekaagaard/linux-surfacebook2-rotation-bugfix.git
-cd linux-surfacebook2-rotation-bugfix
-sudo ./install.sh
+
+    git clone https://github.com/runekaagaard/linux-surfacebook2-rotation-bugfix.git
+    cd linux-surfacebook2-rotation-bugfix
+    ./install.sh
+
+Then log in and out. Rotation of touchpad should now work.
--- linux-surfacebook2-rotation-bugfix.sh
+++ linux-surfacebook2-rotation-bugfix.sh
@@ -5,11 +5,12 @@ MEMBER=MonitorsChanged
 PREV_ROT="UNDEFINED"
 MON="eDP-1"
 TOUCHSCREEN="ipts 045E:0020 Touchscreen"
+echo "Started"
 
 dbus-monitor --profile "interface='$INTERFACE',member='$MEMBER'" |
     while read -r line; do
 	echo $line | grep -v $MEMBER && continue
-	XRANDR=$(xrandr | grep $MON)
+	XRANDR=$(xrandr |2>/dev/null grep $MON)
 	ROT="normal"
 	echo $XRANDR | grep "+0 (" && ROT="normal"
 	echo $XRANDR | grep "+0 right (" && ROT="right"
@@ -21,4 +22,6 @@ dbus-monitor --profile "interface='$INTERFACE',member='$MEMBER'" |
 	[ $ROT == "normal" ]   && xinput set-prop "$TOUCHSCREEN" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0   && echo "Rotation changed to normal"
 	[ $ROT == "inverted" ] && xinput set-prop "$TOUCHSCREEN" --type=float "Coordinate Transformation Matrix" -1 0 1 0 -1 1 0 0 1 && echo "Rotation changed to inverted"
 	PREV_ROT="$ROT"
-    done
+    done &> /dev/null
+
+echo "Stopped"
